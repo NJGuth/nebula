1392f35d2464832425eb5274c16f1fb6d9c49ded
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-01 19:24:55 +1300

    Added has focus for border box

--- src/components/agent/agent-window.tsx
+++ src/components/agent/agent-window.tsx
@@ -18,7 +18,7 @@ export default function AgentWindow() {
           "bg-blue-700 ",
           "hover:bg-blue-800 active:bg-blue-900",
           "data-[state=closed]:hover:scale-105",
-          "data-[state=open]:bg-blue-800 data-[state=open]:ring-4 data-[state=open]:ring-blue-500/40"
+          "data-[state=open]:bg-blue-800 data-[state=open]:ring-4 data-[state=open]:ring-blue-500/40 data-[state=open]:shadow-lg"
         )}
       >
         <AiironSprite className="size-8 rotate-0  transition-all ease-out duration-150 group-data-[state=open]:scale-125 group-data-[state=closed]:hover:scale-105" />
--- src/components/agent/message-input.tsx
+++ src/components/agent/message-input.tsx
@@ -8,15 +8,15 @@ import { AgentButton } from "./agent-button";
 
 export default function MessageInput({ ...props }) {
   return (
-    <div className={cn("p-2 space-y-1 ")}>
-      <div className=" border rounded-md overflow-hidden ">
+    <div className={cn("px-2 pb-2 space-y-1 ")}>
+      <div className=" border rounded-md has-focus:border-blue-700 has-hover:border-blue-700  overflow-hidden peer-focus:ring-2 peer-focus-visible:ring-blue-500/40">
         <AutosizeTextarea
           maxHeight={120}
           minHeight={20}
           className={cn(
             "p-2 ",
-            "placeholder:text-muted-foreground ",
-            "focus-visible:border-none focus-visible:outline-none",
+            "placeholder:text-muted-foreground peer",
+            " focus-visible:outline-none",
             "disabled:cursor-not-allowed disabled:opacity-50"
           )}
           placeholder="Pirates?"
