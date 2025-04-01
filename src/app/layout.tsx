cecc41e83fc667ff5367ca68b79f0b0416f303e0
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-01 19:34:48 +1300

    cleanup pre vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import AgentWindow from "@/components/agent/agent-window";
 
@@ -10,6 +10,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -25,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` ${inter.variable} antialiased`}>
         <main className="relative">
           {children}
           <AgentWindow />
