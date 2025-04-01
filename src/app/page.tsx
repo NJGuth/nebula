fb3dba3318203941c91314bb25770384f60997ae
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-01 15:03:42 +1300

    fix overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen relative">
+    <div className="flex  flex-col items-center justify-center h-screen relative">
       <div
         className={cn(
-          "border w-75 h-[500px] rounded-2xl shadow-md flex flex-col"
+          "border w-75 h-[500px] overflow-hidden rounded-2xl shadow-md flex flex-col"
         )}
       >
         <div className="flex-1 bg-slate-100"></div>
