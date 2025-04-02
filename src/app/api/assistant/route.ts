58482bb42957efa868bf5b9b430bdb7b277d6e13
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-03 12:16:06 +1300

    fix route for deploy

--- src/app/api/assistant/route.ts
+++ src/app/api/assistant/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
 
   return AssistantResponse(
     { threadId, messageId: createdMessage.id },
-    async ({ forwardStream, sendDataMessage }) => {
+    async ({ forwardStream }) => {
       // Run the assistant on the thread
       const runStream = openai.beta.threads.runs.stream(threadId, {
         assistant_id:
@@ -46,9 +46,7 @@ export async function POST(req: Request) {
       ) {
         const tool_outputs =
           runResult.required_action.submit_tool_outputs.tool_calls.map(
-            (toolCall: any) => {
-              const parameters = JSON.parse(toolCall.function.arguments);
-
+            (toolCall: OpenAI.Beta.Threads.Runs.FunctionToolCall) => {
               switch (toolCall.function.name) {
                 // configure your tool calls here
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Agent from "@/components/agent/agent";
 import Assistant from "@/components/agent/assistant";
 
 export default function Home() {
   return (
     <div className="flex  flex-col items-center justify-center h-screen relative">
-      {/* <Agent /> */}
       <Assistant />
     </div>
   );
