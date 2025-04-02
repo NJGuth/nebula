8652e349ce6bd3fdaf502efd0812b262ba236393
Nicholas <Guthrie.nj@gmail.com>
2025-04-02 19:58:14 +1300

    modified chat states and initial message

--- src/components/agent/agent.tsx
+++ src/components/agent/agent.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
+import { cn } from "@/lib/utils";
+import { SendIcon, StopIcon } from "./icons";
+import { AgentButton } from "./agent-button";
 import AgentHeader from "./agent-header";
 import AgentContent from "./agent-content";
-import MessageInput from "./message-input";
 import { AgentMessage, UserMessage } from "./message";
+import { AutosizeTextarea } from "./autosize-textarea";
 
 export default function Agent() {
   const { messages, input, handleInputChange, handleSubmit, status, stop } =
-    useChat();
+    useChat({
+      initialMessages: [
+        {
+          role: "assistant",
+          content:
+            "Hello im Aiiron,\nI am an AI coach, here to support you between your coaching sessions. I can help you prepare for sessions, reflect on your coaching, craft development goals and more, get started by letting me know what you need!",
+          id: "initial",
+        },
+      ],
+    });
   return (
     <div className="border w-85 h-[600px] overflow-hidden rounded-2xl shadow-md flex flex-col">
       <AgentHeader />
@@ -40,7 +52,8 @@ export default function Agent() {
           ))}
         </div>
       </AgentContent>
-      {(status === "submitted" || status === "streaming") && (
+
+      {/* {(status === "submitted" || status === "streaming") && (
         <div>
           {status === "submitted" && <p>Submitted</p>}
           {status === "streaming" && <p>Streaming</p>}
@@ -48,14 +61,39 @@ export default function Agent() {
             Stop
           </button>
         </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <MessageInput
-          value={input}
-          placeholder="Enter your message here..."
-          onChange={handleInputChange}
-        />
-      </form>
+      )} */}
+      <div className={cn("px-2 pb-2 space-y-1 ")}>
+        <div className=" border rounded-md has-focus:border-blue-700 has-hover:border-blue-700  overflow-hidden peer-focus:ring-2 peer-focus-visible:ring-blue-500/40">
+          <form onSubmit={handleSubmit}>
+            <AutosizeTextarea
+              value={input}
+              placeholder="Enter your message here..."
+              onChange={handleInputChange}
+              className={cn(
+                "p-3 ",
+                "placeholder:text-slate-500 peer",
+                " focus-visible:outline-none",
+                "disabled:cursor-not-allowed disabled:opacity-50"
+              )}
+            />
+
+            <div className="flex justify-end p-1 gap-2">
+              <AgentButton
+                variant="submit"
+                type="submit"
+                disabled={!input && status !== "streaming"}
+                onClick={
+                  status === "streaming" || status === "submitted"
+                    ? () => stop()
+                    : undefined
+                }
+              >
+                {status === "streaming" ? <StopIcon /> : <SendIcon />}
+              </AgentButton>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/agent/message-input.tsx
+++ src/components/agent/message-input.tsx
@@ -2,39 +2,28 @@
 
 import { cn } from "@/lib/utils";
 import { AutosizeTextarea } from "./autosize-textarea";
-import { SendIcon } from "./icons";
-import { AgentButton } from "./agent-button";
 
 export default function MessageInput({ ...props }) {
   return (
-    <div className={cn("px-2 pb-2 space-y-1 ")}>
-      <div className=" border rounded-md has-focus:border-blue-700 has-hover:border-blue-700  overflow-hidden peer-focus:ring-2 peer-focus-visible:ring-blue-500/40">
-        <AutosizeTextarea
-          maxHeight={120}
-          minHeight={20}
-          className={cn(
-            "p-3 ",
-            "placeholder:text-slate-500 peer",
-            " focus-visible:outline-none",
-            "disabled:cursor-not-allowed disabled:opacity-50"
-          )}
-          placeholder="What do you want to talk about?"
-          onKeyDown={(e) => {
-            if (e.key === "Return" && !e.shiftKey) {
-              e.preventDefault();
-              if (props.value) {
-                props.onSubmit?.(e);
-              }
-            }
-          }}
-          {...props}
-        />
-        <div className="flex justify-end p-1 gap-2">
-          <AgentButton variant="submit" disabled={!props.value} type="submit">
-            <SendIcon />
-          </AgentButton>
-        </div>
-      </div>
-    </div>
+    <AutosizeTextarea
+      maxHeight={120}
+      minHeight={20}
+      className={cn(
+        "p-3 ",
+        "placeholder:text-slate-500 peer",
+        " focus-visible:outline-none",
+        "disabled:cursor-not-allowed disabled:opacity-50"
+      )}
+      placeholder="What do you want to talk about?"
+      onKeyDown={(e) => {
+        if (e.key === "Return" && !e.shiftKey) {
+          e.preventDefault();
+          if (props.value) {
+            props.onSubmit?.(e);
+          }
+        }
+      }}
+      {...props}
+    />
   );
 }
