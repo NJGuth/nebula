85cb6aa5821896a60840d29d4061c26bce5b4c44
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-03 19:47:28 +1300

    removed messages

--- src/app/api/threads/[threadId]/messages/route.ts
+++ src/app/api/threads/[threadId]/messages/route.ts
@@ -5,20 +5,21 @@ export const runtime = "nodejs";
 // Send a new message to a thread
 export async function POST(
   request: Request,
-  { params }: { params: { threadId: string } }
+  context: { params: { [key: string]: string | string[] } }
 ) {
+  const threadId = context.params.threadId as string;
 
   if (!process.env.OPENAI_ASSISTANT_ID) {
     throw new Error("OPENAI_ASSISTANT_ID environment variable is not set");
   }
   const { content } = await request.json();
 
-  await openai.beta.threads.messages.create(params.threadId, {
+  await openai.beta.threads.messages.create(threadId, {
     role: "user",
     content: content,
   });
 
-  const stream = openai.beta.threads.runs.stream(params.threadId, {
+  const stream = openai.beta.threads.runs.stream(threadId, {
     assistant_id: process.env.OPENAI_ASSISTANT_ID,
   });
 
