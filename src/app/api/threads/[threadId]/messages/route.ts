69b8d0597faf1e080d0639d4d437ad186f2e6d54
Nicholas Guthrie <nicholas@aetherlens.io>
2025-04-03 19:43:33 +1300

    Update route

--- src/app/api/threads/[threadId]/messages/route.ts
+++ src/app/api/threads/[threadId]/messages/route.ts
@@ -1,18 +1,15 @@
+
 import { openai } from "@/app/api/openai";
 
 export const runtime = "nodejs";
 
 // Send a new message to a thread
-export async function POST(
-  request: Request,
-  context: { params: { threadId: string } }
-) {
+export async function POST(request: Request, { params: { threadId } }: { params: { threadId: string } }) {
+
   if (!process.env.OPENAI_ASSISTANT_ID) {
     throw new Error("OPENAI_ASSISTANT_ID environment variable is not set");
   }
-
   const { content } = await request.json();
-  const { threadId } = context.params;
 
   await openai.beta.threads.messages.create(threadId, {
     role: "user",
@@ -25,3 +22,4 @@ export async function POST(
 
   return new Response(stream.toReadableStream());
 }
+
